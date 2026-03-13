d5db915a9df097a48279b5e6183e7d04bd0b3384
chase kirk <kirklubesit30908635@gmail.com>
2026-03-13 19:21:32 -0400

    fix: restore ak-ui.tsx component deleted in f0932a7

---
+++ src/components/ak/ak-ui.tsx
@@ -0,0 +1,13 @@
+﻿"use client";
+
+export function AkShell({ children }: { children: React.ReactNode }) {
+  return <div style={{ padding: 20 }}>{children}</div>;
+}
+
+export function AkPanel({ children }: { children: React.ReactNode }) {
+  return <div style={{ marginBottom: 20 }}>{children}</div>;
+}
+
+export function AkButton({ children }: { children: React.ReactNode }) {
+  return <button>{children}</button>;
+}
